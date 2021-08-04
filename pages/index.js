ddb5dfe9b51068c62597c0d3b6f661a1fce7fff5
Tanisha Chaudhary <tanishach203@gmail.com>
2021-08-05 02:01:21 +0545

    banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../Components/Banner";
 import Header from "../Components/Header";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+      <Banner/>
     </div>
   );
 }
